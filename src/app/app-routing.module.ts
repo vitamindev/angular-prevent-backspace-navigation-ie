830e1d3955f46a46f2b633ed2cca1629dd68114f
benoitpaul <benoit.paul@gmail.com>
2020-05-14 14:16:04 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo: 'page1' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-prevent-backspace-navigation-ie';
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(evt: KeyboardEvent) {
+    if (evt.key === 'Backspace') {
+      let doPrevent = true;
+      const target = evt.target as HTMLInputElement;
+      const disabled = target.disabled || target.readOnly;
+      if (!disabled) {
+        if (target.isContentEditable) {
+          doPrevent = false;
+        }
+
+        const whitelist = ['INPUT', 'TEXTAREA', 'SELECT'];
+
+        const nodeName = target.nodeName.toLocaleUpperCase();
+        if (whitelist.indexOf(nodeName) > -1) {
+          doPrevent = false;
+        }
+      }
+
+      if (doPrevent) {
+        evt.preventDefault();
+        return false;
+      }
+    }
+  }
 }
